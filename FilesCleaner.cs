c95a4eb3388ab2e05984b33225c2507bc67a1c12
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-06-17 13:47:16 +0200

    File cleaner service implemented

--- FilesCleaner.cs
+++ FilesCleaner.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Timers;
 
 namespace RedundantFilesCleanerService
 {
     public class FilesCleaner
     {
+        private readonly System.Timers.Timer _timer;
+        public FilesCleaner()
+        {
+            _timer = new System.Timers.Timer(1000) { AutoReset = true };
+            _timer.Elapsed += voidDeleteFiles;
+        }
+        private void voidDeleteFiles(object sender, ElapsedEventArgs e)
+        {
 
+            DirectoryInfo directoryInfo = new DirectoryInfo("C:\\WebReports\\Estimate Template");
+            var files = directoryInfo.EnumerateFiles();
+
+            foreach (var file in files)
+            {
+                file.Delete();
+            }
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RedundantFilesCleanerService;
+using Topshelf;
+
+var exitCode = HostFactory.Run(x =>
+{
+    x.Service<FilesCleaner>(s =>
+    {
+        s.ConstructUsing(fileCleaner => new FilesCleaner());
+        s.WhenStarted(fileCleaner => fileCleaner.Start());
+        s.WhenStopped(fileCleaner => fileCleaner.Stop());
+    });
+    x.RunAsLocalSystem();
+
+    x.SetServiceName("RedundantFilesCleanerService");
+    x.SetDisplayName("Redundant Files Cleaner Service");
+    x.SetDescription("This service delete redundant files from user specified folders on this machine");
+});
+
+int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+Environment.ExitCode = exitCodeValue;
